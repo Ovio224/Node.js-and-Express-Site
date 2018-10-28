6795788ad16dfeae5beaa5fe891fd0273dc174f3
Ovi# <covidiu97@gmail.com>
2018-10-28 11:34:17 +0100

    Final stage

--- routes/projects.js
+++ routes/projects.js
@@ -4,7 +4,7 @@ const { projects } = require('../data/data.json');
 
 // getting the data from the json file to display it dynamically 
 router.get('/:id', (req, res) => {
-    // const { qid } = req.query;
+    const qid = req.query;
     const { id } = req.params;
     const { description } = projects[id];
     const title = projects[id].project_name;
@@ -12,8 +12,13 @@ router.get('/:id', (req, res) => {
     const { live_link } = projects[id];
     const { github_link } = projects[id];
     const { image_urls } = projects[id];
+    let count = parseInt(id);
+    if (count >= 5) {
+        count = -1;
+    }
 
     const templateData = {
+        count,
         title,
         description,
         technologies,
